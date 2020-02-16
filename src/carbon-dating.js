f9aa0dee15d3d251ab81a6b941fa33204eda69d9
CasperCarver <pigulevsky.gleb@ya.ru>
2020-02-16 20:52:46 +0300

    feat: change task carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,13 +2,20 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(N) {
-  N=parseInt(N, 10); 
-  if ( N >15 || N<15 || N==""){
+  if (typeof N !== "string"){
     return false;
   }
-  t = math.log(MODERN_ACTIVITY/N)/(0.693/HALF_LIFE_PERIOD);
-  return t;
+   N=Number(N); 
+   if ( N >15 || N<0 ){
+      return false;
+   }
+  // t = Math.log(MODERN_ACTIVITY/N)/(0.693/HALF_LIFE_PERIOD);
+  // return Math.ceil(t);
   
-  throw 'Not implemented';
-  // remove line with error and write your code here
 };
+
+// let N=false;
+
+// if (typeof N !== "string"){
+//   console.log (false);
+// } else console.log(true);
