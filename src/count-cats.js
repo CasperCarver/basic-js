85480b3ac670b91f8d714b6ee0a880a7de0bb1cc
CasperCarver <pigulevsky.gleb@ya.ru>
2020-02-16 14:56:52 +0300

    feat: add task count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,13 @@
 module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+  module.exports = function countCats(arr) {
+    let cat ="^^";
+    let count = 0;
+    for (let i=0; i<arr.length;i++){
+      if (arr[i]==cat){
+        count+=1;
+      }
+    }
+    return count>0?count:0;
+    throw 'Not implemented';
+  };
 };
