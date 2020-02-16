eef2e05eb657b7bd87c612a015eec7526e317045
CasperCarver <pigulevsky.gleb@ya.ru>
2020-02-16 19:41:37 +0300

    feat: change task counts cat

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,13 +1,15 @@
-module.exports = function countCats(/* matrix */) {
   module.exports = function countCats(arr) {
     let cat ="^^";
     let count = 0;
     for (let i=0; i<arr.length;i++){
-      if (arr[i]==cat){
-        count+=1;
+      for(let c=0;c<arr[i].length;c++){
+        if (arr[i][c]==cat){
+          count+=1;
+        }
       }
+      
     }
     return count>0?count:0;
-    throw 'Not implemented';
+    //throw 'Not implemented';
   };
-};
+
