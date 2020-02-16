24c03cf93b3d97da17c5e5a5038dc8f7bd7f1ac2
CasperCarver <pigulevsky.gleb@ya.ru>
2020-02-16 16:00:24 +0300

    feat: add task carbon method

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(N) {
+  N=parseInt(N, 10); 
+  if ( N >15 || N<15 || N==""){
+    return false;
+  }
+  t = math.log(MODERN_ACTIVITY/N)/(0.693/HALF_LIFE_PERIOD);
+  return t;
+  
   throw 'Not implemented';
   // remove line with error and write your code here
 };
