42102b55eb2494f689b03a87ba2aa15be09cdd4a
CasperCarver <pigulevsky.gleb@ya.ru>
2020-02-16 22:09:12 +0300

    feat: change task carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,21 +1,45 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+
 
 module.exports = function dateSample(N) {
+  const MODERN_ACTIVITY= 15; 
+const HALF_LIFE_PERIOD= 5730;
   if (typeof N !== "string"){
     return false;
   }
-   N=Number(N); 
-   if ( N >15 || N<0 ){
+  N = N.match(/\d+/g);
+  if (N==null){
+     return false;
+  }
+  N=N.join();
+  N= parseFloat(N); 
+    if ( N >15 || N<0 ){
       return false;
-   }
-  // t = Math.log(MODERN_ACTIVITY/N)/(0.693/HALF_LIFE_PERIOD);
-  // return Math.ceil(t);
+    }
+   let t = Math.log(MODERN_ACTIVITY/N)/(0.693/HALF_LIFE_PERIOD);
+   return Math.ceil(t);
   
 };
 
-// let N=false;
+ let N='9.122605776326203';
+if (typeof N !== "string"){
+  console.log (false);
+} else console.log(true);
+let F = N.match(/\d+\.\d+/g);
+if (F==null){
+  if (N.match(/\d+/g)==null)
+  console.log(false)
+} else
+N=F.join();
+console.log(N);
+N= parseFloat(N);
 
-// if (typeof N !== "string"){
-//   console.log (false);
-// } else console.log(true);
+console.log(typeof(N));
+console.log(N);
+if ( N >15 || N<0  ){
+   console.log(false);
+} else console.log(true);
+const MODERN_ACTIVITY= 15; 
+const HALF_LIFE_PERIOD= 5730;
+
+let t = Math.log(MODERN_ACTIVITY/N)/(0.693/HALF_LIFE_PERIOD);
+  console.log(Math.ceil(t));
