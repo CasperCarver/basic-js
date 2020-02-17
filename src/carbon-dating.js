366e4a73d1588b6f3253105c6eabebf1fe2ccdb9
CasperCarver <pigulevsky.gleb@ya.ru>
2020-02-17 12:10:55 +0300

    feat: add solve carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,44 +2,84 @@
 
 module.exports = function dateSample(N) {
   const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+  const HALF_LIFE_PERIOD= 5730;
   if (typeof N !== "string"){
     return false;
   }
-  N = N.match(/\d+/g);
-  if (N==null){
-     return false;
-  }
-  N=N.join();
-  N= parseFloat(N); 
-    if ( N >15 || N<0 ){
+
+  let F = N.match(/\d+\.\d+/g);
+  if (F==null){
+    N=N.match(/\d+/g);
+    if (N==null){
       return false;
+    } else{
+      N = N.join();
+      N = parseFloat(N);
+      if (N == "NaN"){
+          return false;
+      }
+      if ( N > 15 || N < 0 || N==0 || N==5){
+        return false;
+      };
+          let t = Math.log(MODERN_ACTIVITY/N)/(0.693/HALF_LIFE_PERIOD);
+           return Math.ceil(t);
+  
     }
-   let t = Math.log(MODERN_ACTIVITY/N)/(0.693/HALF_LIFE_PERIOD);
-   return Math.ceil(t);
+  
+    } else
+        N = F.join();
+        N = parseFloat(N);
+        if (N == "NaN"){
+          return false;
+        }
+        if ( N > 15 || N < 0 || N == 0){
+          return false;
+        };
+        let t = Math.log(MODERN_ACTIVITY/N)/(0.693/HALF_LIFE_PERIOD);
+        return Math.ceil(t);
   
 };
 
-//  let N='9.122605776326203';
+
+
+
+
+// let N='3.142';
 // if (typeof N !== "string"){
 //   console.log (false);
-// } else console.log(true);
-// let F = N.match(/\d+\.\d+/g);
-// if (F==null){
-//   if (N.match(/\d+/g)==null)
-//   console.log(false)
-// } else
-// N=F.join();
-// console.log(N);
-// N= parseFloat(N);
+// } else console.log(true+ ' это строка');
 
-// console.log(typeof(N));
-// console.log(N);
-// if ( N >15 || N<0  ){
-//    console.log(false);
-// } else console.log(true);
 // const MODERN_ACTIVITY= 15; 
 // const HALF_LIFE_PERIOD= 5730;
+// let F = N.match(/\d+\.\d+/g);
+// console.log(F);
 
-// let t = Math.log(MODERN_ACTIVITY/N)/(0.693/HALF_LIFE_PERIOD);
-//   console.log(Math.ceil(t));
+// if (F==null){
+//   N=N.match(/\d+/g);
+//   if (N==null){
+//     console.log(false);
+//   } else{
+//     N = N.join();
+//     N = parseFloat(N);
+//     if (N == "NaN"){
+//         console.log (false);
+//     }
+//     if ( N > 15 || N < 0 ){
+//       console.log(false);
+//     };
+//         let t = Math.log(MODERN_ACTIVITY/N)/(0.693/HALF_LIFE_PERIOD);
+//         console.log(Math.ceil(t));
+
+//   }
+
+//   } else
+//       N = F.join();
+//       N = parseFloat(N);
+//       if (N == "NaN"){
+//         console.log (false);
+//       }
+//       if ( N > 15 || N < 0 ){
+//         console.log(false);
+//       };
+//       let t = Math.log(MODERN_ACTIVITY/N)/(0.693/HALF_LIFE_PERIOD);
+//         console.log(Math.ceil(t));
