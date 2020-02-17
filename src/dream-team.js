e9efad091efcb2d03f0fa881760b4e0d232891d1
CasperCarver <pigulevsky.gleb@ya.ru>
2020-02-17 15:50:36 +0300

    feat: add solve dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,45 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+   if (!Array.isArray(arr)){
+    return false;
+  }
+  let N =[];
+
+for (let i = 0; i<arr.length; i++){
+  if (typeof arr[i] == 'string'){
+    arr[i]=arr[i].trim().toUpperCase();
+    N.push(arr[i][0]);
+
+  } else continue;
+  
+}
+N.sort();
+N=N.join('');
+return N;
+ 
+   //throw 'Not implemented';
+  // // remove line with error and write your code here
 };
+
+
+//  let arr = ['   William Alston ',
+//         ' Paul Benacerraf',
+//         '  Ross Cameron',
+//         '       Gilles Deleuze',
+//         '  Arda Denkel ',
+//         '  Michael Devitt',
+//         '  Kit Fine',
+//         ' Nelson Goodman',
+//         'David Kolb',
+//         '   Saul Kripke',
+//         '  Trenton Merricks',
+//         '  Jay Rosenberg',];
+// let N =[];
+// for (let i = 0; i<arr.length; i++){
+//   arr[i]=arr[i].trim();
+//   console.log(arr[i]);
+//   N.push(arr[i][0]);
+// }
+// N.sort();
+// N=N.join('');
+
+// console.log(N);
