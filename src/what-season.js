e6d5433360cf755d5ebf38b1530b940452cba58f
CasperCarver <pigulevsky.gleb@ya.ru>
2020-02-17 21:11:59 +0300

    feat: add solve of what-season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,28 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
+module.exports = function getSeason(d) {
+  if (!d){
+     return 'Unable to determine the time of year!';
+  };
+  dm=d.getMonth();
+  if (dm>=2 && dm<=4){
+    return  'spring';
+  } else if ( dm>=5 && dm<=7){
+    return 'summer';
+  } else if ( dm>=8 && dm<=10){
+    return 'autumn';
+  } return'winter';
+  
+  // throw 'Not implemented';
   // remove line with error and write your code here
 };
+
+
+// let d= new Date(1682, 0, 28, 4, 2, 55, 833);
+// dm=d.getMonth();
+// if (dm>=2 && dm<=4){
+//   console.log ('spring');
+// } else if ( dm>=5 && dm<=7){
+//   console.log ('summer');
+// } else if ( dm>=8 && dm<=10){
+//   console.log('autumn');
+// } else console.log('winter');
+// console.log(dm);
