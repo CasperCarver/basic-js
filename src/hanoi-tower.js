14a10b482dd5885e63e4dda53a983127b35882f6
CasperCarver <pigulevsky.gleb@ya.ru>
2020-02-29 21:25:58 +0300

    feat: add solve task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,23 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  //  throw 'Not implemented';
+    let obj={
+        
+    };
+
+    obj.turns=Math.pow(2,disksNumber)-1;
+    obj.seconds=obj.turns/(turnsSpeed/3600);
+    return obj;
+    
+    
     // remove line with error and write your code here
 }
+// let disksNumber=414,
+//     turnsSpeed=19279842;
+
+// let obj={
+    
+// };
+// //turnsSpeed=turnsSpeed/3600;
+// obj.turns=Math.pow(2,disksNumber)-1;
+// obj.seconds=obj.turns*3600/turnsSpeed;
+// console.log(obj);
